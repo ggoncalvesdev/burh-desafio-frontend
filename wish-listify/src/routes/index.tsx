224789840b687bab9d4d33c58ed202bdca954574
Gabriel Gonçalves <92227976+ggoncalvesdev@users.noreply.github.com>
2023-05-24 19:44:12 -0300

    feat(Router): separating logged user routes

--- wish-listify/src/routes/index.tsx
+++ wish-listify/src/routes/index.tsx
@@ -1,13 +1,23 @@
 import { Route, Routes } from "react-router";
 import { Home } from "../pages/Home";
-import { Register } from "../pages/Register";
+import { Register } from "../pages/Forms/register";
+import { Login } from "../pages/Forms/login";
 
 export const Router = () => {
-  return (
+  const storedEmail = localStorage.getItem("email");
+  if (storedEmail !== null) {
+    return (
       <Routes>
-        <Route path="/" element={<Register />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Visializar" element={<Home />} />
       </Routes>
-  );
+    );
+  } else {
+    return (
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    );
+  }
 };
