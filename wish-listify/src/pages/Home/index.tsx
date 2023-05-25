8f8dc85916e526b3e1065fb21269146eabac765c
Gabriel Gonçalves <92227976+ggoncalvesdev@users.noreply.github.com>
2023-05-24 23:39:33 -0300

    feat(Home): initial implementation

--- wish-listify/src/pages/Home/index.tsx
+++ wish-listify/src/pages/Home/index.tsx
@@ -2,14 +2,129 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../../styles/global";
 import { defaultTheme } from "../../styles/themes/default";
 
-import { ContentContainer} from "./styles";
+import { ContentContainer, GridContainer, Title } from "./styles";
+import { List } from "../../components/List";
+import { ListProps, ListWishes } from "../../model/ListProps";
+
 
 export const Home = () => {
-  return(
+  const userWishLists: ListProps[] = [
+    {
+      type: "Curto Prazo",
+      title: "Lista de Desejos de Livros",
+      wishes: [
+        { name: "Em Busca do Tempo Perdido", description: "Marcel Proust" },
+        { name: "Ulysses", description: "James Joyce" },
+        { name: "Dom Quixote", description: "Miguel de Cervantes" },
+        { name: "O Grande Gatsby ", description: "F. Scott Fitzgerald" },
+        { name: "Cem Anos de Solidão", description: "Gabriel García Márquez" },
+        { name: "Moby Dick", description: "Herman Melville" },
+        { name: "Guerra e Paz", description: "Lev Tolstói" },
+        { name: "Lolita", description: "Vladimir Nabokov" },
+      ],
+    },
+    {
+      type: "Médio Prazo",
+      title: "Lista de Desejos de Eletrônicos",
+      wishes: [
+        { name: "Em Busca do Tempo Perdido", description: "Marcel Proust" },
+        { name: "Ulysses", description: "James Joyce" },
+        { name: "Dom Quixote", description: "Miguel de Cervantes" },
+        { name: "O Grande Gatsby ", description: "F. Scott Fitzgerald" },
+        { name: "Cem Anos de Solidão", description: "Gabriel García Márquez" },
+        { name: "Moby Dick", description: "Herman Melville" },
+        { name: "Guerra e Paz", description: "Lev Tolstói" },
+        { name: "Lolita", description: "Vladimir Nabokov" },
+      ],
+    },
+    {
+      type: "Longo Prazo",
+      title: "Lista de Desejos de Viagens",
+      wishes: [
+        { name: "Em Busca do Tempo Perdido", description: "Marcel Proust" },
+        { name: "Ulysses", description: "James Joyce" },
+        { name: "Dom Quixote", description: "Miguel de Cervantes" },
+        { name: "O Grande Gatsby ", description: "F. Scott Fitzgerald" },
+        { name: "Cem Anos de Solidão", description: "Gabriel García Márquez" },
+        { name: "Moby Dick", description: "Herman Melville" },
+        { name: "Guerra e Paz", description: "Lev Tolstói" },
+        { name: "Lolita", description: "Vladimir Nabokov" },
+      ],
+    },
+    {
+      type: "Médio Prazo",
+      title: "Lista de Desejos de Viagens",
+      wishes: [
+        { name: "Em Busca do Tempo Perdido", description: "Marcel Proust" },
+        { name: "Ulysses", description: "James Joyce" },
+        { name: "Dom Quixote", description: "Miguel de Cervantes" },
+        { name: "O Grande Gatsby ", description: "F. Scott Fitzgerald" },
+        { name: "Cem Anos de Solidão", description: "Gabriel García Márquez" },
+        { name: "Moby Dick", description: "Herman Melville" },
+        { name: "Guerra e Paz", description: "Lev Tolstói" },
+        { name: "Lolita", description: "Vladimir Nabokov" },
+      ],
+    },
+    {
+      type: "Longo Prazo",
+      title: "Lista de Desejos de Viagens",
+      wishes: [
+        { name: "Em Busca do Tempo Perdido", description: "Marcel Proust" },
+        { name: "Ulysses", description: "James Joyce" },
+        { name: "Dom Quixote", description: "Miguel de Cervantes" },
+        { name: "O Grande Gatsby ", description: "F. Scott Fitzgerald" },
+        { name: "Cem Anos de Solidão", description: "Gabriel García Márquez" },
+        { name: "Moby Dick", description: "Herman Melville" },
+        { name: "Guerra e Paz", description: "Lev Tolstói" },
+        { name: "Lolita", description: "Vladimir Nabokov" },
+      ],
+    },
+    {
+      type: "Médio Prazo",
+      title: "Lista de Desejos de Viagens",
+      wishes: [
+        { name: "Em Busca do Tempo Perdido", description: "Marcel Proust" },
+        { name: "Ulysses", description: "James Joyce" },
+        { name: "Dom Quixote", description: "Miguel de Cervantes" },
+        { name: "O Grande Gatsby ", description: "F. Scott Fitzgerald" },
+        { name: "Cem Anos de Solidão", description: "Gabriel García Márquez" },
+        { name: "Moby Dick", description: "Herman Melville" },
+        { name: "Guerra e Paz", description: "Lev Tolstói" },
+        { name: "Lolita", description: "Vladimir Nabokov" },
+      ],
+    },
+    {
+      type: "Curto Prazo",
+      title: "Lista de Desejos de Viagens",
+      wishes: [
+        { name: "Em Busca do Tempo Perdido", description: "Marcel Proust" },
+        { name: "Ulysses", description: "James Joyce" },
+        { name: "Dom Quixote", description: "Miguel de Cervantes" },
+        { name: "O Grande Gatsby ", description: "F. Scott Fitzgerald" },
+        { name: "Cem Anos de Solidão", description: "Gabriel García Márquez" },
+        { name: "Moby Dick", description: "Herman Melville" },
+        { name: "Guerra e Paz", description: "Lev Tolstói" },
+        { name: "Lolita", description: "Vladimir Nabokov" },
+      ],
+    },
+  ];
+
+  return (
     <ThemeProvider theme={defaultTheme}>
-    <GlobalStyle />
-    <ContentContainer>
-    </ContentContainer>
+      <GlobalStyle />
+      <ContentContainer>
+        <Title>Minhas Listas de Desejos</Title>
+        <GridContainer>
+          {userWishLists.map((wishList, index) => (
+            <List
+              key={index}
+              type={wishList.type}
+              title={wishList.title}
+              wishes={wishList.wishes.map((wish) => wish )}
+            />
+          ))}
+        </GridContainer>
+      </ContentContainer>
     </ThemeProvider>
-  )
-}
+  );
+};
--- wish-listify/src/pages/Home/styles.ts
+++ wish-listify/src/pages/Home/styles.ts
@@ -2,4 +2,40 @@ import styled from 'styled-components'
 
 export const ContentContainer = styled.main`
   min-width: 100%;
+  min-height: 100vh;
+  background-color: ${(props) => props.theme['gray-800']};
+`
+export const GridContainer = styled.div`
+  max-width: 1080px;
+  margin: auto;
+  display: grid;
+
+  padding: 60px 0;
+  justify-content: center;
+  grid-template-columns: repeat(auto-fit, 250px);
+  grid-gap: 80px;
+
+  img {
+    width: 250px;
+    max-height: 360px;
+    object-fit: contain;
+  }
+
+  @media screen and (max-width: 510px) {
+    grid-template-columns: repeat(auto-fit, 180px);
+    img {
+      width: 180px;
+    }
+  }
+
+  @media (max-width: 510px) {
+      justify-self: center;
+      grid-gap: 100px;
+  }
+
+`
+export const Title = styled.h1`
+  padding-top: 100px;
+  text-align: center;
+  color: ${(props) => props.theme.white};
 `
